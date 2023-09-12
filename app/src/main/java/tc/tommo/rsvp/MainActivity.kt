eaab4d212758b380ca438a83cc732383fc2ec01e
Tommo <59585282+tc-tommo@users.noreply.github.com>
2023-09-12 12:54:14 +0100

    add hawking

--- app/src/main/java/tc/tommo/rsvp/MainActivity.kt
+++ app/src/main/java/tc/tommo/rsvp/MainActivity.kt
@@ -105,7 +105,10 @@ fun ScreenContent() {
             ),
             keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
             keyboardActions = KeyboardActions(
-                onDone = { keyboardController?.hide() }
+                onDone = {
+                    keyboardController?.hide();
+
+                }
             )
         )
     }
