20ba36aacb3b6590805314d4aff0db9772717692
Tommo <59585282+tc-tommo@users.noreply.github.com>
2023-09-12 12:49:47 +0100

    Initial commit

---
+++ app/src/main/java/tc/tommo/rsvp/MainActivity.kt
@@ -0,0 +1,112 @@
+package tc.tommo.rsvp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import tc.tommo.rsvp.ui.theme.SendEventTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScreenContent()
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+@Preview
+@Composable
+fun ScreenContent() {
+    var whatText  by remember { mutableStateOf(TextFieldValue()) }
+    var whenText  by remember { mutableStateOf(TextFieldValue()) }
+    var whereText by remember { mutableStateOf(TextFieldValue()) }
+    val focusManager = LocalFocusManager.current
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        TextField(
+            value = whatText,
+            onValueChange = { whatText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            textStyle = MaterialTheme.typography.bodyMedium,
+            label = { Text("What?") },
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = MaterialTheme.colorScheme.surface
+            ),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            )
+        )
+        TextField(
+            value = whenText,
+            onValueChange = { whenText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            textStyle = MaterialTheme.typography.bodyMedium,
+            label = { Text("When?") },
+            colors = TextFieldDefaults.textFieldColors(
+                   textColor = MaterialTheme.colorScheme.surface
+            ),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            keyboardActions = KeyboardActions(
+                onNext = { focusManager.moveFocus(FocusDirection.Down) }
+            )
+        )
+        TextField(
+            value = whereText,
+            onValueChange = { whereText = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(all = 16.dp),
+            textStyle = MaterialTheme.typography.bodyMedium,
+            label = { Text("Where?") },
+            colors = TextFieldDefaults.textFieldColors(
+                textColor = MaterialTheme.colorScheme.surface
+            ),
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = { keyboardController?.hide() }
+            )
+        )
+    }
+}
